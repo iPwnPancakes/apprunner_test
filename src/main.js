cb59ef9aad108d55a31410c64126c2bedaac91df
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2024-11-15 16:32:20 -0600

    Removed node modules

--- src/main.js
+++ src/main.js
@@ -1,9 +1,40 @@
 import express from 'express';
+import knex from 'knex';
 
 const app = express();
 
+console.log('before knex instantiation I think');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: {
+    host: 'database-1.cbswyei8e50w.us-west-2.rds.amazonaws.com',
+    port: 5432,
+    user: 'postgres',
+    password: 'password',
+    database: 'ass',
+    ssl: false
+  }
+});
+
+console.log('after knex instantiation');
+
 app.get('/', (req, res) => {
   res.end('Hello World');
+});
+
+app.get('/fuck', async (req, res) => {
+  console.log('before query');
+
+  try {
+    await knexInstance.raw('SELECT 1');
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+
+  console.log('after query');
+
 });
 
 app.get('/error', (req, res) => {
@@ -13,17 +44,3 @@ app.get('/error', (req, res) => {
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function example() {
-  console.log('Start sleep');
-
-  await sleep(300000);
-
-  throw new Error('holy shmoley');
-}
-
-example();
